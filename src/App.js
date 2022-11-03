a09d8b67c17096c38638e108c232e15d67c8aaf8
Lalit Thakre <100556995+lalithakre@users.noreply.github.com>
2022-11-03 22:25:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Lalit</h1>
+      <h2>Start learning React!</h2>
+    </div>
+  );
+}
